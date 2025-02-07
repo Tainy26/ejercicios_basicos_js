187d3f2ff56acd87696cce79a2945e94401483c7
Carlos Daniel <cdntapia@icloud.com>
2025-02-08 00:13:52 +0100

    actualizado

---
+++ ejercicio3.js
@@ -0,0 +1,31 @@
+// Ejercicio 3
+// En este ejercicio podéis mostrar directamente los resultados por consola o 
+// almacenarlos en variables (esto lo podéis hacer en todos los demás, como os 
+// resulte más claro).
+
+// 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+let primerEjercicio = 10 * 5;
+
+// 3.2 Divide 10 por 2 y muestra el resultado en un console.
+
+let segundoEjercicio = 10 / 2;
+
+// 3.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+let tercerEjercicio = 15 % 9;
+
+// 3.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+let cuartoEjercicio = p + j;
+
+// 3.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+let c = 10;
+let m = 5;
+let quintoEjercicio = c * m;
+
+
+console.log("Los resultados de los ejercicios son: " + primerEjercicio, segundoEjercicio, tercerEjercicio, cuartoEjercicio, quintoEjercicio);
